1317d61adf050effe5d369efb5ae4147d1eae09a
jmcs811 <justincaseyy69@gmail.com>
2019-06-11 16:33:23 -0400

    starting week 4. 8 puzzle project.

---
+++ 8Puzzle/src/Board.java
@@ -0,0 +1,35 @@
+public class Board {
+
+  public Board(int[][] blocks) {}
+
+  public int hamming(){
+    return 0;
+  }
+
+  public int manhattan() {
+    return 0;
+  }
+
+  public boolean isGoal() {
+    return false;
+  }
+
+  public Board twin() {
+
+    return null;
+  }
+
+  public boolean equals(Object y) {
+    return false;
+  }
+
+  public Iterable<Board> neighbors() {
+
+    return null;
+  }
+
+  public String toString() {
+
+    return null;
+  }
+}
